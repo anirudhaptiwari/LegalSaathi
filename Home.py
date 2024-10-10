fc326bc9fc26a55eb54cd37829a258bf2baa1aa3
Anirudha Pradumna Tiwari <114850796+anirudhaptiwari@users.noreply.github.com>
2024-10-10 20:42:13 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import time
 
-# Set page configuration
 st.set_page_config(
     page_title="Legal Saathi",
     page_icon="⚖️",
